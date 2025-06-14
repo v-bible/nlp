08ce8b3c7cdba62e527d0f538afd659cc31d635a
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2025-06-14 09:34:36 +0700

    fix(lib): update remark parsing

--- src/lib/remark.ts
+++ src/lib/remark.ts
@@ -1,6 +1,7 @@
 import rehypeParse from 'rehype-parse';
 import rehypeRemark from 'rehype-remark';
 import rehypeRemoveComments from 'rehype-remove-comments';
+import remarkGfm from 'remark-gfm';
 import remarkStringify from 'remark-stringify';
 import { unified } from 'unified';
 
@@ -9,6 +10,7 @@ const parseMd = async (html: string) => {
     .use(rehypeParse)
     .use(rehypeRemoveComments)
     .use(rehypeRemark)
+    .use(remarkGfm)
     .use(remarkStringify)
     .process(html);
 
