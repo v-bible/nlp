a2e76f44ad328417020e67fa9860c7d0b1fb3ed4
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2025-06-19 15:44:27 +0700

    feat(conggiao.org): add scripts

---
+++ src/conggiao.org/main.ts
@@ -0,0 +1,38 @@
+import { getPageContent } from '@/conggiao.org/getPageContent';
+import { getPageContentMd } from '@/conggiao.org/getPageContentMd';
+import { Crawler } from '@/lib/nlp/crawler';
+
+const main = async () => {
+  const crawler = new Crawler({
+    name: 'conggiao.org',
+    domain: 'R',
+    subDomain: 'C',
+    getMetadataBy: (metadataRow) => {
+      return (
+        metadataRow.source === 'conggiao.org' &&
+        metadataRow.sourceType === 'web'
+      );
+    },
+    filterCheckpoint: (checkpoint) => {
+      // REVIEW: Currently we get non chapter pages first
+      return !checkpoint.completed && !checkpoint.params.hasChapters;
+    },
+    getChapters: async ({ resourceHref }) => {
+      // NOTE: These pages have no chapters
+      return [
+        {
+          href: resourceHref.href,
+          props: {
+            chapterNumber: 1,
+          },
+        },
+      ];
+    },
+    getPageContent,
+    getPageContentMd,
+  });
+
+  await crawler.run();
+};
+
+main();
